71d519575c9176d95a3c5e50778a923e43b73a7e
anmolab <99890311+anmolab@users.noreply.github.com>
2024-02-03 20:31:29 +0530

    .

--- server.js
+++ server.js
@@ -2,15 +2,25 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 
-
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT, () => {
-    console.log(`Listensing on port ${PORT}`)
+    console.log(`Listening on port ${PORT}`)
 })
 
-app.use(express.static(__dirname  + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
 })
